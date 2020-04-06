575a71888c7dbe6faae18a31fd8d6ae022ecde4a
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-06 14:10:14 +0700

    #13 - Add Fields in Main App

--- src/main/java/com/hendisantika/springbootemailthymeleaftemplate/SpringbootEmailThymeleafTemplateApplication.java
+++ src/main/java/com/hendisantika/springbootemailthymeleaftemplate/SpringbootEmailThymeleafTemplateApplication.java
@@ -1,10 +1,18 @@
 package com.hendisantika.springbootemailthymeleaftemplate;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootEmailThymeleafTemplateApplication {
+
+    private static final String[] RECIPIENTS = {"email@konohagakure.co.jp"};
+    private static final String SUBJECT = "Spring Boot Email Thymelef Template";
+
+    @Autowired
+    private MailService mailService;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootEmailThymeleafTemplateApplication.class, args);
