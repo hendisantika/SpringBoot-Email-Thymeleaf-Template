8b0e4a7876a0eddccfe4e0a989ef1edb47eefb44
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-06 14:10:54 +0700

    #14 - Add sendNormalTextMessage in Main App

--- src/main/java/com/hendisantika/springbootemailthymeleaftemplate/SpringbootEmailThymeleafTemplateApplication.java
+++ src/main/java/com/hendisantika/springbootemailthymeleaftemplate/SpringbootEmailThymeleafTemplateApplication.java
@@ -1,11 +1,12 @@
 package com.hendisantika.springbootemailthymeleaftemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootEmailThymeleafTemplateApplication {
+public class SpringbootEmailThymeleafTemplateApplication implements CommandLineRunner {
 
     private static final String[] RECIPIENTS = {"email@konohagakure.co.jp"};
     private static final String SUBJECT = "Spring Boot Email Thymelef Template";
@@ -18,4 +19,20 @@ public class SpringbootEmailThymeleafTemplateApplication {
         SpringApplication.run(SpringbootEmailThymeleafTemplateApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        sendNormalTextMessage();
+        sendHtmlTemplateMessage();
+        sendHtmlTemplateMessageWithInlineImage_imageFileName();
+        sendHtmlTemplateMessageWithInlineImage_imageByteArray();
+    }
+
+    private void sendNormalTextMessage() {
+        try {
+            mailService.sendMailText(RECIPIENTS, SUBJECT, "Ez egy lev�l t�rzs", new String[]{"proba.txt"});
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
 }
